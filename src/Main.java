67ba9ae7a1330df02a77d6b3ec6f8a182883e60c
filippov2912 <filippov2912@mail.ru>
2023-05-01 16:09:13 +0300

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -138,8 +138,8 @@ public class Main {
         double mashaDifferencePerYear = (mashaSalaryAfterPerYear - mashaSalaryBeforePerYear);
         double denisDifferencePerYear = (denisSalaryAfterPerYear - denisSalaryBeforePerYear);
         double christinaDifferencePerYear = (christinaSalaryAfterPerYear - christinaSalaryBeforePerYear);
-        System.out.println("Маша теперь получает " + mashaSalaryAfterPerMonth + " рублей в месяц." + "Годовой доход вырос на " + mashaDifferencePerYear + " рублей");
-        System.out.println("Денис теперь получает " + denisSalaryAfterPerMonth + " рублей в месяц." + "Годовой доход вырос на " + denisDifferencePerYear + " рублей");
+        System.out.println("Маша теперь получает " + mashaSalaryAfterPerMonth + " рублей в месяц;" + "Годовой доход вырос на " + mashaDifferencePerYear + " рублей");
+        System.out.println("Денис теперь получает " + denisSalaryAfterPerMonth + " рублей в месяц;" + "Годовой доход вырос на " + denisDifferencePerYear + " рублей");
         System.out.println("Кристина теперь получает " + christinaSalaryAfterPerMonth + " рублей в месяц." + "Годовой доход вырос на " + christinaDifferencePerYear + " рублей");
     }
 }
